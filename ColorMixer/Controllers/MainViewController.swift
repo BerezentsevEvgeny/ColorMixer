6db20a6d88b657c8107d11e35b8f83457dc8afd0
Berezentsev Evgeny <jonn09@icloud.com>
2021-07-25 22:24:21 +0300

    a

--- ColorMixer/Controllers/MainViewController.swift
+++ ColorMixer/Controllers/MainViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate {
+    func setViewColor(with color: UIColor)
+    func setSliderValue(red: Float)
+}
+
 class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    var redValue: Float?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.delegate = self
+        settingsVC.newColor = view.backgroundColor
+        guard let redValue = redValue else { return }
+        settingsVC.red = redValue
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
     }
 
+}
+
+extension MainViewController: SettingsViewControllerDelegate {
+    func setSliderValue(red: Float) {
+        redValue = red
+    }
+ 
+    
+    func setViewColor(with color: UIColor) {
+        view.backgroundColor = color
+    }
+    
 
 }
--- ColorMixer/Controllers/SettingsViewController.swift
+++ ColorMixer/Controllers/SettingsViewController.swift
@@ -7,49 +7,70 @@
 
 import UIKit
 
-//protocol SettingsViewControllerDelegate {
-//    func setViewColor(with color:)
-//}
+
 
 class SettingsViewController: UIViewController {
     
     @IBOutlet weak var coloredView: UIView!
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    @IBOutlet weak var redSliderValue: UILabel!
-    @IBOutlet weak var greenSliderValue: UILabel!
-    @IBOutlet weak var blueSliderValue: UILabel!
+    @IBOutlet weak var redSliderLabel: UILabel!
+    @IBOutlet weak var greenSliderLabel: UILabel!
+    @IBOutlet weak var blueSliderLabel: UILabel!
+    
+    var delegate: SettingsViewControllerDelegate?
+    var newColor: UIColor?
+    var red: Float?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         coloredView.layer.cornerRadius = 10
-        navigationController?.isNavigationBarHidden = true
-        setColor()
-        updateSliderValues()
+        coloredView.backgroundColor = newColor
+        updateSliderLabels()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let red = red else { return }
+        setSliderValues(red: red)
     }
 
     @IBAction func sliderValueChanged() {
-        updateSliderValues()
-        setColor()
+        updateSliderLabels()
+        updateColor()
+    }
+    @IBAction func doneButtonTapped() {
+        let colorToSet = UIColor(red: CGFloat(redSlider.value),
+                                 green: CGFloat(greenSlider.value),
+                                 blue: CGFloat(blueSlider.value),
+                                 alpha: 1)
+        delegate?.setViewColor(with: colorToSet)
+        delegate?.setSliderValue(red: redSlider.value)
     }
     
-    private func updateSliderValues() {
-        redSliderValue.text = string(from: redSlider)
-        greenSliderValue.text = string(from: greenSlider)
-        blueSliderValue.text = string(from: blueSlider)
+    private func updateSliderLabels() {
+        redSliderLabel.text = string(from: redSlider)
+        greenSliderLabel.text = string(from: greenSlider)
+        blueSliderLabel.text = string(from: blueSlider)
     }
     
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
     
-    private func setColor() {
+    private  func updateColor() {
         coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                               green: CGFloat(greenSlider.value),
                                               blue: CGFloat(blueSlider.value),
                                               alpha: 1)
+    }
+    
+    func setSliderValues(red: Float) {
+        redSlider.value = red
+//        greenSlider.value = green
+//        blueSlider.value = blue
     }
     
 
